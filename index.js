fca363de88dc1cc96a9e62553a8115e603c587c6
ananya92 <1992ananya@gmail.com>
2020-01-09 13:52:48 +1030

    Created index.js and created the HTML string literal with variables to fill the portfolio details

---
+++ index.js
@@ -0,0 +1,14 @@
+const details = `
+<h1>Introduction</h1>
+<p>${introduction}</p>
+<h1>Where I live</h1>
+<p>${location}</p>
+<h1>My dream job</h1>
+<p>${dream_job}</p>
+<h1>My favorite hobbies</h1>
+<p>${hobbies}</p>
+<h1>My LinkedIn page</h1>
+<p>${linkedin}</p>
+<h1>My Github page</h1>
+<p>${github}</p>
+`;
